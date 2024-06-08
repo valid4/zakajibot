9ec27ed0d823bd9659a3d3ef0cc64ff4b38bf2fa
Vladislav <osipovvlad46@gmail.com>
2024-06-08 20:53:42 +0500

    update main, create modul settings and setting.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram import F
